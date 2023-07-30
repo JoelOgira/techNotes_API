72dd346d9b82990443f5f8477d07d59091ecdee4
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-07-30 22:19:37 +0300

    Added users route

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/root'));
+app.use('/users', require('./routes/userRoutes'));
 
 app.all('*', (req, res) => {
     res.status(404);
